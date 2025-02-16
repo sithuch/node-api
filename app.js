c88ea5677ba3851f94fabf759590413139fd5793
sithuch <chhemsithuch1999@gmail.com>
2025-02-16 22:38:00 +0700

    install ejs and learning how to used ejs

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ const express = require('express');    // this is a third party module
 
 // express app
 const app = express();
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<p>home page</p>');
-    // res.sendFile('./views/index.html', { root: __dirname });
+    // res.send('<p>home page</p>');
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    // res.send('<p>about page</p>');
+    res.render('about');    
+});
+// redirect
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404');
 });
