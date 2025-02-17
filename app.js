754a6c2ace62da521f4d1785e6687117d606d4ed
sithuch <chhemsithuch1999@gmail.com>
2025-02-17 22:34:20 +0700

    create header and footer file

--- app.js
+++ app.js
@@ -9,13 +9,23 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<p>home page</p>');
-    res.render('index', {title: 'Homie'});
+    
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p>about page</p>');
     res.render('about',{title: 'About'});    
+});
+
+// blog routes
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create a new blog'});
 });
 // // redirect
 // app.get('/about-us', (req, res) => {
