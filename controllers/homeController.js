250647cbebd29d3cca2e7beab8e02d098c1d702c
sithuch <chhemsithuch1999@gmail.com>
2025-04-26 11:55:32 +0700

    create new home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,7 @@
 exports.homePage = (req, res) => {
   res.render('index', { title: 'Home' });
 }
+
+exports.indexPage = (req, res) => {
+  res.render('homePage', { title: 'Home Page' });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 const homeController = require('../controllers/homeController');
-
 // Define routes for the home page
 routes.get('/', homeController.homePage);
 
+routes.get('/home', homeController.indexPage);
 module.exports = routes;
