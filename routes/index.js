1edd28c5c7c7611002b6c2628800441043f1e855
sithuch <chhemsithuch1999@gmail.com>
2025-05-01 23:16:46 +0700

    create page register

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const routes = express.Router();
 const homeController = require('../controllers/homeController');
 // Define routes for the home page
-routes.get('/', homeController.homePage);
+routes.get('/register', homeController.homePage);
 
 routes.get('/home', homeController.indexPage);
+module.exports = routes;
+
+routes.get('/login', homeController.logInPage);
 module.exports = routes;
